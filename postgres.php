edb6f2d64a200a6d5ef9d59198a13cc348f42ca6
Mattia <36816061+MattiaBLX@users.noreply.github.com>
2018-11-16 11:13:38 +0100

    add license

--- postgres.php
+++ postgres.php
@@ -1,4 +1,7 @@
 <?php
+/*This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.*/
 if($TGBot->settings['adminPostGreSQL']){
     try{
         //Grazie ad alsogamer per avermi aiutato con la creazione della tabella che non funzionava 
